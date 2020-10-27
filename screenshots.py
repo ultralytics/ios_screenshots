fb91d0ba09fb556069533686028e68462544e626
Glenn Jocher <glenn.jocher@ultralytics.com>
2020-10-27 17:24:01 +0100

    PIL save

--- screenshots.py
+++ screenshots.py
@@ -1,10 +1,13 @@
 import glob
 import os
+from pathlib import Path
+
+from PIL import Image
 
 from utils.utils import *
 
 # directory of images to resize
-dir = '/Users/glennjocher/Downloads/app'
+dir = '/Users/glennjocher/Downloads/'
 
 # Formats: iPhone 8, iPhone XS, iPad Pro 12.9
 formats = ['5_5', '5_8', '12_9', '6_5']  # (inches)
@@ -27,7 +30,8 @@ def main():
 
             new_name = dir + new_dir + '/' + format + '_' + image.split('/')[-1]
 
-            cv2.imwrite(new_name.replace('.PNG', '.jpg').replace('.png', '.jpg'), img_resized)
+            # cv2.imwrite(new_name.replace('.PNG', '.jpg').replace('.png', '.jpg'), img_resized)  # cv2 save
+            Image.fromarray(img_resized).save(new_name.replace(Path(new_name).suffix, '.jpg'))  # PIL save
 
 
 if __name__ == '__main__':
