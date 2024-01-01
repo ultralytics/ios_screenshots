45b3cffb4521b101cfc694644725e431af89151d
glenn-jocher <glenn.jocher@ultralytics.com>
2024-01-01 02:05:17 +0000

    Auto-format by Ultralytics actions

--- screenshots.py
+++ screenshots.py
@@ -7,32 +7,32 @@ from PIL import Image
 from utils.utils import *
 
 # directory of images to resize
-dir = '/Users/glennjocher/Downloads/'
+dir = "/Users/glennjocher/Downloads/"
 
 # Formats: iPhone 8, iPhone XS, iPad Pro 12.9
-formats = ['5_5', '5_8', '12_9', '6_5']  # (inches)
+formats = ["5_5", "5_8", "12_9", "6_5"]  # (inches)
 size_x = [1242, 1125, 2048, 1242]
 size_y = [2208, 2436, 2732, 2688]
 
 
 def main():
-    new_dir = 'screenshots'
-    images = glob.glob(dir + '/*.*')
+    new_dir = "screenshots"
+    images = glob.glob(dir + "/*.*")
 
-    os.system('rm -rf ' + dir + new_dir)
-    os.system('mkdir ' + dir + new_dir)
+    os.system("rm -rf " + dir + new_dir)
+    os.system("mkdir " + dir + new_dir)
     for image in images:
         img = cv2.imread(image)
         print(image)
 
         for i, format in enumerate(formats):
-            img_resized = resize_image(img, height=size_y[i], width=size_x[i], mode='Fill')  # i.e. mode = 'Pad'
+            img_resized = resize_image(img, height=size_y[i], width=size_x[i], mode="Fill")  # i.e. mode = 'Pad'
 
-            new_name = dir + new_dir + '/' + format + '_' + image.split('/')[-1]
+            new_name = dir + new_dir + "/" + format + "_" + image.split("/")[-1]
 
             # cv2.imwrite(new_name.replace('.PNG', '.jpg').replace('.png', '.jpg'), img_resized)  # cv2 save
-            Image.fromarray(img_resized[..., ::-1]).save(new_name.replace(Path(new_name).suffix, '.jpg'))  # PIL save
+            Image.fromarray(img_resized[..., ::-1]).save(new_name.replace(Path(new_name).suffix, ".jpg"))  # PIL save
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- utils/utils.py
+++ utils/utils.py
@@ -1,14 +1,14 @@
 import cv2
 
 
-def resize_image(img, height=416, width=416, mode='Fill', color=(255, 255, 255)):
+def resize_image(img, height=416, width=416, mode="Fill", color=(255, 255, 255)):
     # mode Fill resizes + clips a rectangular image to a square
     # mode Fit resizes a rectangular image to a padded square
 
     shape = img.shape[:2]  # shape = [height, width]
     ratio_h, ratio_w = float(height) / shape[0], float(width) / shape[1]
 
-    if mode == 'Fill':
+    if mode == "Fill":
         ratio = max(ratio_h, ratio_w)
     else:  # 'Fit'
         ratio = min(ratio_h, ratio_w)
@@ -23,4 +23,4 @@ def resize_image(img, height=416, width=416, mode='Fill', color=(255, 255, 255))
     if (dw > 0) or (dh > 0):  # add border
         return cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)
     else:  # crop to [height, width]
-        return img[abs(top):img.shape[0] - abs(bottom), abs(left):img.shape[1] + abs(left)]
+        return img[abs(top) : img.shape[0] - abs(bottom), abs(left) : img.shape[1] + abs(left)]
